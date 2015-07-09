59e26dd2574b71a0a2621a51161c454d1b90ff60
Soumith Chintala <soumith@fb.com>
2015-07-09 15:48:43 -0500

    adding example luarc and small readme typo fix

---
+++ luarc.lua
@@ -0,0 +1,12 @@
+{
+  control_port = 8080,
+  repl_port = 8081,
+  ssl = false,
+  ssl_dir = "./ssl/server.pem",
+  login = {user = "foo", password="bar"},
+  torch = true,
+  trepl = {globals=false, color=true, timer=false},
+  images = true,
+  keeplines = 5000,
+  shortcuts = {io="io.write"}
+}
